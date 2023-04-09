7bc8b1346384a3b0a356ae7c1f31b16eb445cdd6
Kazuki_Kamizuru <k.amago0721@gmail.com>
2023-04-09 12:38:45 +0900

    feat: add page for profile and contact

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,8 @@
   import "../app.css";
 </script>
 
-<slot />
+<div class="flex justify-center">
+  <div class="container max-w-screen-xl px-8 py-4 md:px-4">
+    <slot />
+  </div>
+</div>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,15 @@
+<div style="color: #f00; font-size: 1.2em">※ご注意ください</div>
+<ul class="posts">
+<li>入力されたメールアドレスが「k2ss.info」ドメインからのメールを受信できるよう設定をお願いします。
+<li>お問い合わせの内容については、お返事に時間がかかることがあります。</li>
+<li>「一緒に仕事してみたい」「こんなこと相談していいのかな…」というお問合せもお待ちしております。</li>
+</ul>
+
+<div style="color: #f00; font-size: 1.2em">※Notes</div>
+<ul class="posts">
+<li>Please make settings so that the input mail address can receive mail from "k2ss.info" domain.</li>
+<li>Depending on contents of contact, I sometimes take time to reply.</li>
+</ul>
+
+<!-- TODO -->
+
