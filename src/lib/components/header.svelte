1cde3bf916d3ad39fdb3024514fe50abb8f29346
Kazuki_Kamizuru <k.amago0721@gmail.com>
2023-04-09 22:21:01 +0900

    feat: show site name

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { SITE_NAME } from '$lib/constants';
 	import { fly } from 'svelte/transition';
 	import Fa from 'svelte-fa/src/fa.svelte';
 	import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
@@ -37,68 +38,73 @@
 	];
 </script>
 
-<header class="relative w-full h-8 mt-4 mb-2 flex items-stretch justify-end">
-	<a
-		class="w-9 flex items-center justify-center"
-		target="_blank"
-		rel="noopener noreferrer"
-		href="https://github.com/gotoeveryone/"
-	>
-		<Fa icon={faTwitter} size="lg" />
-	</a>
-	<a
-		class="w-9 flex items-center justify-center"
-		target="_blank"
-		rel="noopener noreferrer"
-		href="https://twitter.com/kzkamago0721"
-	>
-		<Fa icon={faGithub} size="lg" />
-	</a>
-	<button class="w-9 flex items-center justify-center" on:click={toggleMenu}>
-		<Fa icon={faBars} size="lg" />
-	</button>
-	{#if isOpen}
-		<ul
-			transition:fly={{ duration: 500 }}
-			class="absolute min-w-[200px] m-0 p-0 top-8 right-0 bg-gray-600 border border-black rounded"
+<header class="w-full h-8 mt-4 mb-2 flex items-center justify-between">
+	<div>
+		<a class="font-bold text-xl no-underline hover:underline" href="/">{SITE_NAME}</a>
+	</div>
+	<div class="relative flex items-stretch justify-center">
+		<a
+			class="w-9 flex items-center justify-center"
+			target="_blank"
+			rel="noopener noreferrer"
+			href="https://github.com/gotoeveryone/"
 		>
-			<li class="list-none m-0 hover:bg-gray-700">
-				<a class="px-4 py-3 block" on:click={toggleMenu} href="/">最近の記事</a>
-			</li>
-			<li class="list-none m-0">
-				<ul class="m-0 p-0">
-					{#each postItems as item}
-						<li class="list-none m-0 pl-2 hover:bg-gray-700">
-							<a
-								class="px-4 py-2 block"
-								on:click={toggleMenu}
-								href={`/archives/category/${item.slug}`}>{item.text}</a
-							>
-						</li>
-						{#if item.subItems.length > 0}
-							<li class="list-none m-0">
-								<ul class="m-0 p-0">
-									{#each item.subItems as subItem}
-										<li class="list-none m-0 pl-4 hover:bg-gray-700">
-											<a
-												class="px-4 py-2 block"
-												on:click={toggleMenu}
-												href={`/archives/category/${item.slug}/${subItem.slug}`}>{subItem.text}</a
-											>
-										</li>
-									{/each}
-								</ul>
+			<Fa icon={faTwitter} size="lg" />
+		</a>
+		<a
+			class="w-9 flex items-center justify-center"
+			target="_blank"
+			rel="noopener noreferrer"
+			href="https://twitter.com/kzkamago0721"
+		>
+			<Fa icon={faGithub} size="lg" />
+		</a>
+		<button class="w-9 flex items-center justify-center" on:click={toggleMenu}>
+			<Fa icon={faBars} size="lg" />
+		</button>
+		{#if isOpen}
+			<ul
+				transition:fly={{ duration: 500 }}
+				class="absolute min-w-[200px] m-0 p-0 top-8 right-0 bg-gray-600 border border-black rounded"
+			>
+				<li class="list-none m-0 hover:bg-gray-700">
+					<a class="px-4 py-3 block" on:click={toggleMenu} href="/">最近の記事</a>
+				</li>
+				<li class="list-none m-0">
+					<ul class="m-0 p-0">
+						{#each postItems as item}
+							<li class="list-none m-0 pl-2 hover:bg-gray-700">
+								<a
+									class="px-4 py-2 block"
+									on:click={toggleMenu}
+									href={`/archives/category/${item.slug}`}>{item.text}</a
+								>
 							</li>
-						{/if}
-					{/each}
-				</ul>
-			</li>
-			<li class="list-none m-0 hover:bg-gray-700">
-				<a class="px-4 py-3 block" on:click={toggleMenu} href="/profile">Profile</a>
-			</li>
-			<li class="list-none m-0 hover:bg-gray-700">
-				<a class="px-4 py-3 block" on:click={toggleMenu} href="/contact">Contact</a>
-			</li>
-		</ul>
-	{/if}
+							{#if item.subItems.length > 0}
+								<li class="list-none m-0">
+									<ul class="m-0 p-0">
+										{#each item.subItems as subItem}
+											<li class="list-none m-0 pl-4 hover:bg-gray-700">
+												<a
+													class="px-4 py-2 block"
+													on:click={toggleMenu}
+													href={`/archives/category/${item.slug}/${subItem.slug}`}>{subItem.text}</a
+												>
+											</li>
+										{/each}
+									</ul>
+								</li>
+							{/if}
+						{/each}
+					</ul>
+				</li>
+				<li class="list-none m-0 hover:bg-gray-700">
+					<a class="px-4 py-3 block" on:click={toggleMenu} href="/profile">Profile</a>
+				</li>
+				<li class="list-none m-0 hover:bg-gray-700">
+					<a class="px-4 py-3 block" on:click={toggleMenu} href="/contact">Contact</a>
+				</li>
+			</ul>
+		{/if}
+	</div>
 </header>
