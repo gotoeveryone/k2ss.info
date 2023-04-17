bfafa47de5f63421d847481056fe80f0e939a452
Kazuki_Kamizuru <k.amago0721@gmail.com>
2023-04-17 09:22:17 +0900

    feat: move to component about analytics

---
+++ src/lib/components/analytics.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { dev } from '$app/environment';
+</script>
+
+<svelte:head>
+	{#if !dev}
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-S573SSFVY6"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag() {
+				dataLayer.push(arguments);
+			}
+			gtag('js', new Date());
+
+			gtag('config', 'G-S573SSFVY6');
+		</script>
+	{/if}
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-	import { getMetaTitle, getOpenGraph, getTwitter } from '$lib/modules/meta';
+	import Analytics from '$lib/components/analytics.svelte';
 	import Footer from '$lib/components/footer.svelte';
 	import Header from '$lib/components/header.svelte';
+	import { getMetaTitle, getOpenGraph, getTwitter } from '$lib/modules/meta';
 	import { MetaTags } from 'svelte-meta-tags';
 	import '../app.css';
 </script>
 
 <MetaTags title={getMetaTitle()} openGraph={getOpenGraph()} twitter={getTwitter()} />
+
+<Analytics />
 
 <div class="flex justify-center">
 	<div class="max-w-full md:max-w-screen-xl px-8">
