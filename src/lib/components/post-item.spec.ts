de4e7667b8a4d689996c9e0dc537de0e8d6bb417
Kazuki_Kamizuru <k.amago0721@gmail.com>
2023-05-21 23:57:14 +0900

    test: add workflow for unit test

---
+++ src/lib/components/post-item.spec.ts
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/svelte';
+import { describe, expect, test } from 'vitest';
+import Component from '$lib/components/post-item.svelte';
+
+const item = {
+	id: 1,
+	date: '2006-01-02T15:04:05+09:00',
+	title: 'test title',
+	excerpt: 'test excerpt'
+};
+
+describe('PostItem', () => {
+	test('タイトルが設定されている', () => {
+		const { queryByText } = render(Component, {
+			props: { item }
+		});
+		expect(!!queryByText(item.title)).toBe(true);
+	});
+	test('a タグに記事ページへのリンクが設定されている', () => {
+		const { container } = render(Component, {
+			props: { item }
+		});
+		expect(container.querySelector('a')?.getAttribute('href')).toBe(`/archives/${item.id}/`);
+	});
+});
