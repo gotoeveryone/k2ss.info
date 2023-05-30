dfb12e1667c0152e9be3dc37b27b9f984d4611dc
Kazuki_Kamizuru <k.amago0721@gmail.com>
2023-05-30 18:46:27 +0900

    feat: adjust layout for header and drawer

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,3 +1,4 @@
 export const SITE_NAME = '@K2SS - 日々、邁進';
+export const SITE_DESCRIPTION = '碁打ちエンジニアの雑記';
 export const TWITTER_ACCOUNT = 'kzkamago0721';
 export const GITHUB_ACCOUNT = 'gotoeveryone';
