3440ea060b90a37ca8d160519dcf03b11ad61969
Kazuki_Kamizuru <k.amago0721@gmail.com>
2023-06-11 20:38:27 +0900

    refactor: remove unnecessary if

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,13 +3,7 @@ import { Post as PostRepo } from '$lib/repositories/post';
 export const load = async () => {
 	const postRepo = new PostRepo();
 	const { items: posts } = await postRepo.getPosts({ page: 1, perPage: 20 });
-	if (posts.length) {
-		return {
-			posts
-		};
-	}
-
 	return {
-		posts: []
+		posts,
 	};
 };
