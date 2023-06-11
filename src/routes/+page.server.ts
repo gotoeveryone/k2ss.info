aaa91e0b411f02257e5102b89fce1b67ba9ff16e
Kazuki_Kamizuru <k.amago0721@gmail.com>
2023-06-11 20:42:59 +0900

    fix: format

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,6 +4,6 @@ export const load = async () => {
 	const postRepo = new PostRepo();
 	const { items: posts } = await postRepo.getPosts({ page: 1, perPage: 20 });
 	return {
-		posts,
+		posts
 	};
 };
