3ee346233cdc9a17e2c7da1bd3570cc9cf6e07cc
Kazuki_Kamizuru <k.amago0721@gmail.com>
2023-09-05 10:20:24 +0900

    deps: upgrade to svelte4

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
+		watch: false,
 		globals: true,
 		environment: 'jsdom',
 		include: ['src/**/*.{test,spec}.{js,ts}']
