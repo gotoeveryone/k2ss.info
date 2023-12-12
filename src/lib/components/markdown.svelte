8c3abfc5a5b3ec9a3b892aeaa8108590b6aec077
Kazuki_Kamizuru <k.amago0721@gmail.com>
2023-12-12 22:56:48 +0900

    feat: upgrade marked

---
+++ src/lib/components/markdown.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { marked } from 'marked';
+
+	const renderer = new marked.Renderer();
+	renderer.link = (href, title, text) => {
+		const target = href.startsWith('http') ? '_blank' : '_self';
+		return `<a href="${href}" title="${title || ''}" target="${target}">${text}</a>`;
+	};
+
+	export let content: string;
+</script>
+
+{@html marked(content, { renderer })}
