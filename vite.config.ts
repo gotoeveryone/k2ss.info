d3fa68d2e82fde8edd4cfab5b4b2f3f62b04b6ea
Kazuki_Kamizuru <k.amago0721@gmail.com>
2025-04-15 14:24:03 +0900

    deps: upgrade vite and svelte

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
-import type { UserConfig } from "vite";
-import type { InlineConfig } from "vitest";
-
-interface VitestConfigExport extends UserConfig {
-  test: InlineConfig;
-}
+import { svelteTesting } from '@testing-library/svelte/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), svelteTesting()],
 	test: {
 		watch: false,
 		globals: true,
 		environment: 'jsdom',
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}'],
 	}
-} as VitestConfigExport);
+});
