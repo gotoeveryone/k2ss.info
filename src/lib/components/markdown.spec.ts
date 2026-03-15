de99ead0ae470c40d92f3f627095ec58ebf4a2ec
Kazuki_Kamizuru <k.amago0721@gmail.com>
2026-03-15 17:03:32 +0900

    fix: render nested markdown links correctly

--- src/lib/components/markdown.spec.ts
+++ src/lib/components/markdown.spec.ts
@@ -19,4 +19,18 @@ describe("Markdown", () => {
 		expect(link?.getAttribute("target")).toBe("_blank");
 		expect(link?.getAttribute("rel")).toBe("noopener noreferrer");
 	});
+
+	test("画像リンクが文字列ではなく img 要素として描画される", () => {
+		const { container } = render(Component, {
+			props: {
+				content:
+					"[![](/images/2022/08/20220829_1.png)](/images/2022/08/20220829_1.png)",
+			},
+		});
+
+		const link = container.querySelector("a");
+		const image = container.querySelector("img");
+		expect(link?.getAttribute("href")).toBe("/images/2022/08/20220829_1.png");
+		expect(image?.getAttribute("src")).toBe("/images/2022/08/20220829_1.png");
+	});
 });
